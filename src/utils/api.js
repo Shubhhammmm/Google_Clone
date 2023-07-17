92e7cc3a889b21aab6c232e8bd3d53beb519c028
Shubham Shrivastava <128039947+Shubhhammmm@users.noreply.github.com>
2023-07-17 13:04:25 +0530

    Add files via upload

---
+++ src/utils/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const BASE_URL = "https://www.googleapis.com/customsearch/v1";
+
+const params = {
+    key : "AIzaSyBOycGDnVA5ZXGfquiADd6iFV66zEKM-1M",
+    cx : "8614d901bf87749df",
+}
+
+
+export const fetchDataFromApi = async (payload) => {
+     const {data} = await axios.get(BASE_URL,{
+      params : {...params, ...payload}   
+    })
+    return data;
+};
