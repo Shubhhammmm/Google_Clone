e1b406e540419b4c1b5f596969eed0e30b4db77c
Shubham Shrivastava <128039947+Shubhhammmm@users.noreply.github.com>
2023-07-25 18:45:05 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL = "https://www.googleapis.com/customsearch/v1";
 
 const params = {
-    key : "AIzaSyBOycGDnVA5ZXGfquiADd6iFV66zEKM-1M",
+    key : 'f4f35a30a8mshc251b32922a74bep18d893jsn60a258327c97',
     cx : "8614d901bf87749df",
 }
 
